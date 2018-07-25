569ccbdc667457d0bcc6b138c32c644b4001ebf4
Julissa <demouser@MacBook-Pro.local>
2018-07-24 23:44:06 -0700

    update eleven forty-three

--- javaScript/start.js
+++ javaScript/start.js
@@ -4,6 +4,10 @@ window.onload = function (){
 
   $(".page-1").click(function(){
       $(".display div").hide();
-      $(".display .page-2").first().show();
+      $(".display .page-2").first().show();}
+
+  $(".page-2").click(function(){
+      $(".display div").hide();
+      $(".display .page-3").first().show();
   });
 }
