f218882c653dfd3ab320caf385fcd572941f3f76
Miggle <demouser@MacBook-Pro.local>
2018-07-26 11:20:13 -0700

    The Big Boi

--- javaScript/start.js
+++ javaScript/start.js
@@ -32,20 +32,40 @@ window.onload = function (){
       $(".display div").hide();
       $(".display .page-7").first().show();
     });
-    $("#page-6-2").click(function(){
-        $(".display div").hide();
-        $(".display .page-71").first().show();
-        });
+  $("#page-6-2").click(function(){
+      $(".display div").hide();
+      $(".display .page-71").first().show();
+    });
   $("#page-7-1").click(function(){
       $(".display div").hide();
-      $(".display .page-8").first().show();
+      $(".display .page-9").first().show();
       });
-  $("#page-8-1").click(function(){
+  $("#page-7-1-1").click(function(){
       $(".display div").hide();
       $(".display .page-9").first().show();
+      });
+  $("#page-8-2").click(function(){
+      $(".display div").hide();
+      $(".display .page-8").first().show();
       });
   $("#page-4-2").click(function(){
       $(".display div").hide();
       $(".display .page-11").first().show();
+      });
+  $("#page-4-2-2").click(function(){
+      $(".display div").hide();
+      $(".display .page-11").first().show();
+      });
+  $("#page-4-2-2-2").click(function(){
+      $(".display div").hide();
+      $(".display .page-11").first().show();
+      });
+  $("#page-11-1").click(function(){
+      $(".display div").hide();
+      $(".display .page-12").first().show();
+      });
+  $("#page-81-1").click(function(){
+      $(".display div").hide();
+      $(".display .page-121").first().show();
       });
 }
